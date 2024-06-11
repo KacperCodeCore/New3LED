42fcc40133ffa7c92984c6740402f498d9701988
KacperCodeCore <94679062+KacperCodeCore@users.noreply.github.com>
2024-06-11 09:59:09 +0200

    before fix

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -21,9 +21,30 @@
 #include "dma.h"
 #include "tim.h"
 #include "gpio.h"
+#include <stdlib.h>
+#include "ws2812b.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+const uint8_t gamma8[] = {
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
+    1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
+    2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
+    5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
+   10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+   17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+   25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+   37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+   51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+   69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+   90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
+  115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
+  144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
+  177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
+  215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 };
+
+
 
 /* USER CODE END Includes */
 
@@ -93,21 +114,21 @@ int main(void)
 
 
   /* USER CODE BEGIN 2 */
-  HAL_TIM_Base_Start(&htim3);
-
-  uint8_t test[40 + 7 * 24 + 1] = {0};
-
-  // Zerowanie kolorow wszystkich diod
-  for (int i = 0; i < 7 * 24; i++)
-    test[40 + i] = 32;
-
-  // Wlącz jedną diodę
-  test[40] = 64;
-
-  // Stan wysoki na koncu
-  test[40 + 7 * 24] = 100;
-
-  HAL_TIM_PWM_Start_DMA(&htim3, TIM_CHANNEL_1, &test, sizeof(test));
+//  HAL_TIM_Base_Start(&htim3);
+//
+//  uint8_t test[40 + 7 * 24 + 1] = {0};
+//
+//  // Zerowanie kolorow wszystkich diod
+//  for (int i = 0; i < 7 * 24; i++)
+//    test[40 + i] = 32;
+//
+//  // Wlącz jedną diodę
+//  test[40] = 64;
+//
+//  // Stan wysoki na koncu
+//  test[40 + 7 * 24] = 100;
+//
+//  HAL_TIM_PWM_Start_DMA(&htim3, TIM_CHANNEL_1, &test, sizeof(test));
 
 
 
@@ -115,10 +136,20 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+  ws2812b_init();
+
   while (1)
   {
     /* USER CODE END WHILE */
+	  uint8_t r = gamma8[rand() % 256];
+	  uint8_t g = gamma8[rand() % 256];
+	  uint8_t b = gamma8[rand() % 256];
 
+	  for (int led = 0; led < 6; led++) {
+	    ws2812b_set_color(led, r, g, b);
+	    ws2812b_update();
+	    HAL_Delay(100);
+	  }
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
